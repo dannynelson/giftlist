a35be02550c7e4344d7b1701332d7cac59b405de
Christian Monaghan <christian.monaghan@gmail.com>
2014-02-22 11:06:22 -0800

    Added browse tab with slide functionality

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -39,6 +39,12 @@ angular.module('starter.controllers', [])
 })
 
 
+.controller('BrowseCtrl', function($scope, GiftService) {
+  // "Gifts" is a service returning mock data (services.js)
+  $scope.gifts = GiftService.all();
+})
+
+
 // A simple controller that fetches a list of data from a service
 .controller('GiftIndexCtrl', function($scope, GiftService) {
   // "Gifts" is a service returning mock data (services.js)
