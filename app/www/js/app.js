8172d69d51c4540ab388d64a100d9a2aacea810d
Christian Monaghan <christian.monaghan@gmail.com>
2014-02-22 13:19:01 -0800

    Inject firebase

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.services', 'starter.controllers', 'starter.directives'])
+angular.module('starter', ['ionic', 'starter.services', 'starter.controllers', 'starter.directives', 'firebase'])
 
 
 .config(function($stateProvider, $urlRouterProvider) {
