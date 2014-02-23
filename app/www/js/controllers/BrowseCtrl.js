abf040e30ebf2f080389103a9c99878c0e09eae4
Wayne Montague <wayne.montague@gmail.com>
2014-02-22 22:11:52 -0800

    attempted to fix the swipe issue

--- app/www/js/controllers/BrowseCtrl.js
+++ app/www/js/controllers/BrowseCtrl.js
@@ -3,6 +3,7 @@ angular.module('giftlist.controllers')
 .controller('BrowseCtrl', function($scope, GiftService) {
   // "Gifts" is a service returning mock data (services.js)
   $scope.gifts = GiftService.all();
+  // console.log($scope.gifts);
 
   $scope.removeGift = function(swipeDir) {
     // pop gift from the potential gift list
@@ -10,9 +11,11 @@ angular.module('giftlist.controllers')
     // the gift is either tossed away (swipe left) or
     // stored in wishlist (swipe right)
 
-    console.log("remove gift", swipeDir);
-    console.log($scope.gifts.shift());
+    // var gift = $scope.gifts.shift();
+    // console.log(gift);
+    if (swipeDir === "right") {
+      $scope.wishList.push(gift);
+    }
     console.log($scope.gifts);
-    //
   };
 });
