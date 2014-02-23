344ff9136aa95f4592b09990eda393f3e9d2471a
Wayne Montague <wayne.montague@gmail.com>
2014-02-22 17:37:52 -0800

    added logic to remove a gift from the gifts collection on swipe

--- app/www/js/controllers/BrowseCtrl.js
+++ app/www/js/controllers/BrowseCtrl.js
@@ -5,6 +5,14 @@ angular.module('giftlist.controllers')
   $scope.gifts = GiftService.all();
 
   $scope.removeGift = function(swipeDir) {
+    // pop gift from the potential gift list
+    // depending on the direction of the swipe
+    // the gift is either tossed away (swipe left) or
+    // stored in wishlist (swipe right)
+
     console.log("remove gift", swipeDir);
+    console.log($scope.gifts.shift());
+    console.log($scope.gifts);
+    //
   };
 });
